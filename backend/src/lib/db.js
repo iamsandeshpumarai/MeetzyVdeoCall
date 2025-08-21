27520de4192f8304006be5e738c9b60af5b5e5c0
Sandesh RaI <pumasandeshrai@gmail.com>
2025-08-21 09:05:54 +0545

    Update db.js
    delted the url

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://parmilarai750:JIT4kTExdxrKg5cq@cluster0.lwpqiar.mongodb.net/meetzy_db?retryWrites=true&w=majority&appName=Cluster0");
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(mongodburl);
+    
   } catch (error) {
     console.log("Error in connecting to MongoDB", error);
     process.exit(1); // 1 means failure
